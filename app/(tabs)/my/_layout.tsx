393fabdb5a949ea49e01b1e9941a4c064d7c3cb7
yerimi00 <fhddl1019@naver.com>
2025-08-18 22:03:38 +0900

    feat(my-answer) : 답변하기 토클 구현

--- app/(tabs)/my/_layout.tsx
+++ app/(tabs)/my/_layout.tsx
@@ -1,5 +1,15 @@
 import { Stack } from "expo-router";
+import { View } from "react-native";
+import "react-native-reanimated";
 
 export default function MyStackLayout() {
-  return <Stack screenOptions={{ headerShown: false }} />;
+  return (
+    <View style={{ flex: 1 }}>
+      <Stack
+        screenOptions={{
+          headerShown: false,
+        }}
+      ></Stack>
+    </View>
+  );
 }
