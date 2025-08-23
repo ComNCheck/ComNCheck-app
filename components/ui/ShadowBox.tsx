f0d31c447ae36751b763a1c011f6f6a6ace898ff
yerimi00 <fhddl1019@naver.com>
2025-08-23 18:14:44 +0900

    hotfix : ShadowBox 컴포넌트 수정

--- components/ui/ShadowBox.tsx
+++ components/ui/ShadowBox.tsx
@@ -2,6 +2,8 @@ import { View } from "react-native";
 
 export default function ShadowBox({ children }: { children: React.ReactNode }) {
   return (
-    <View className="flex-1 bg-white rounded-xl p-4 shadow-md">{children}</View>
+    <View className="flex-1 bg-white rounded-xl p-4 border-[#E5E7EB] border-[1px]">
+      {children}
+    </View>
   );
 }
