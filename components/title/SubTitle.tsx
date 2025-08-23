4c291bda204e9460fbd848cb3a314182e418b436
sungwonnoh <jessica20730@naver.com>
2025-08-23 18:28:39 +0900

    hotfix: 헤더바 패딩 수정

--- components/title/SubTitle.tsx
+++ components/title/SubTitle.tsx
@@ -6,7 +6,7 @@ interface SubTitleProps {
 }
 export default function SubTitle({ title, description }: SubTitleProps) {
   return (
-    <View className="flex-col items-start gap-1 my-5">
+    <View className="flex-col items-start gap-1 my-2">
       <Text className="font-semibold text-xl text-text">{title}</Text>
       <Text className="font-medium text-lg text-[#b6b6b6]">
         {String(description).replace(/\\n/g, "\n")}
