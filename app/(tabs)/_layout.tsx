44ba1775e1c93581ed2d0870a37b1660077cf906
sungwonnoh <jessica20730@naver.com>
2025-10-25 17:05:53 +0900

    fix: 다크모드 제거

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -5,16 +5,13 @@ import { HapticTab } from "@/components/HapticTab";
 import { IconSymbol } from "@/components/ui/IconSymbol";
 import TabBarBackground from "@/components/ui/TabBarBackground";
 import { Colors } from "@/constants/Colors";
-import { useColorScheme } from "@/hooks/useColorScheme";
 import { FontAwesome, MaterialCommunityIcons } from "@expo/vector-icons";
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+        tabBarActiveTintColor: Colors.light.tint,
         headerShown: false,
         tabBarButton: HapticTab,
         tabBarBackground: TabBarBackground,
